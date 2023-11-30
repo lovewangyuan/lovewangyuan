5392df08d1cb4f674462e23ec508957ef3f43f98
lovewangyuan <2237539784@qq.com>
2023-11-30 12:18:21 +0800

    自动推送

--- mytools.py
+++ mytools.py
@@ -99,23 +99,25 @@ def 制作交叉表(数据表, 自变量, 因变量):
     return pd.crosstab(数据表[自变量], 数据表[因变量], normalize='columns', margins=True)
 
 
-import pandas as pd
-from scipy import stats
 
-def 单变量参数估计(file_path,confidence_level):
+def 计算单变量均值的置信区间(数据表路径及文件名,变量名,置信水平=0.95):
+    """计算指定数据表中数值变量的均值及在指定置信水平下的置信区间"""
+
+    import pandas as pd
+    from scipy import stats
 # 打开数据文件
-  file_path = R"data/movie_data_cleaned.csv"
-  df_movies = pd.read_csv(file_path)
+    file_path = 数据表路径及文件名
+    df = pd.read_csv(file_path)
  # 计算均值和标准误差
-  mean = df_movies['average'].mean()
-  std_error = stats.sem(df_movies['average'])
+    mean = df[变量名].mean()
+    std_error = stats.sem(df[变量名])
 # 设定置信水平
-  confidence_level = 0.95
+    confidence_level = 置信水平
 # 设定自由度
-  自由度 = len(df_movies['average']) - 1
+    自由度 = len(df[变量名]) - 1
 # 计算置信区间
-  confidence_interval = stats.t.interval(confidence_level, 自由度, loc=mean, scale=std_error)
+    confidence_interval = stats.t.interval(confidence_level, 自由度, loc=mean, scale=std_error)
 # 输出结果
-  print(F"均值：{mean: .2f}")
-  print(F"均值在置信水平{confidence_level}下的置信区间为：", confidence_interval)
-   
+    print(F"变量{变量名}均值：{mean: .2f}")
+    print(F"均值在置信水平{confidence_level}下的置信区间为：", confidence_interval)
+    return mean,confidence_interval
